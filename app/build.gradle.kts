d50850de23f0ceaf8d36cc5d62cb0effc877f9ff
Dariushhh <dariusheine@gmx.net>
2025-02-02 22:03:21 +0100

    New try to setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "dariuuuushh.dariusallerlei"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dariuuuushh.dariusallerlei"
