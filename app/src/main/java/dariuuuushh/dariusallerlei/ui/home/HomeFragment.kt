ba4280fe6eda622600541c58a4c1ba4906a46593
Dariushhh <dariusheine@gmx.net>
2025-02-04 23:04:49 +0100

    Design ToDo-TableRow first step

--- app/src/main/java/dariuuuushh/dariusallerlei/ui/home/HomeFragment.kt
+++ app/src/main/java/dariuuuushh/dariusallerlei/ui/home/HomeFragment.kt
@@ -1,17 +1,14 @@
 package dariuuuushh.dariusallerlei.ui.home
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.EditText
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.checkbox.MaterialCheckBox
@@ -44,37 +41,56 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
 
+        val nameTextInput: TextInputLayout = binding.nameTextInput
+        nameTextInput.apply {
+            setPadding(16, 16, 16, 16)
+            hint = "Test"
+        }
+
         val toDoTable: TableLayout = binding.toDoList
-        val firstToDo = binding.firstToDo
-        val firstAddButton = binding.firstAddButton
-        firstAddButton.setOnClickListener{addNewTodoRow()}
+        val addTableRow = binding.addTableRow
+        val todoAddButton = binding.todoAddButton
+        todoAddButton.setOnClickListener{
+            addNewTodoRow()
+            val index = toDoTable.indexOfChild(addTableRow)
+            if (index < toDoTable.childCount - 1) {
+                toDoTable.removeViewAt(index)
+                toDoTable.addView(addTableRow, index + 1)
+            }
+        }
         return root
     }
 
     private fun addNewTodoRow() {
         val toDoTable: TableLayout = binding.toDoList
-        val newRow = TableRow(requireContext()).apply {  setPadding(16, 16, 16, 16) }
+        val addTableRow = binding.addTableRow
 
-        val todoTextInputLayout = TextInputLayout(requireContext(), null, com.google.android.material.R.attr.textInputOutlinedStyle).apply {
-            layoutParams = TableRow.LayoutParams(
-                TableRow.LayoutParams.MATCH_PARENT,
-                TableRow.LayoutParams.WRAP_CONTENT,
-                1f
-            )
-            hint = "Neue Aufgabe"
-        }
+        val inflater = LayoutInflater.from(requireContext())
+        val newRow = inflater.inflate(R.layout.todo_table_row, toDoTable, false) as TableRow
 
-        val todoTextInputEditText = TextInputEditText(requireContext()).apply {
-            textSize = 18f
-        }
+        val todoTextInputLayout = newRow.findViewById<TextInputLayout>(R.id.todo_input_layout)
+        // val todoTextInputEditText = newRow.findViewById<TextInputEditText>(R.id.todo_input_edit_text)
 
-        todoTextInputLayout.addView(todoTextInputEditText)
-        val todoCheckBox = MaterialCheckBox(requireContext())
-
-        newRow.addView(todoCheckBox)
-        newRow.addView(todoTextInputLayout)
+        todoTextInputLayout.startIconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_start_icon)
 
         toDoTable.addView(newRow)
+
+        todoTextInputLayout.setStartIconOnClickListener {
+            todoTextInputLayout.startIconDrawable = null
+            todoTextInputLayout.post {
+                todoTextInputLayout.endIconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_end_icon)
+            }
+
+            Toast.makeText(requireContext(), "Start icon clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        todoTextInputLayout.setEndIconOnClickListener {
+            todoTextInputLayout.endIconDrawable = null
+            todoTextInputLayout.post {
+                todoTextInputLayout.startIconDrawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_start_icon)
+            }
+            Toast.makeText(requireContext(), "End icon clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/dariuuuushh/dariusallerlei/ui/home/HomeViewModel.kt
+++ app/src/main/java/dariuuuushh/dariusallerlei/ui/home/HomeViewModel.kt
@@ -1,6 +1,5 @@
 package dariuuuushh.dariusallerlei.ui.home
 
-import android.widget.TableLayout
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
